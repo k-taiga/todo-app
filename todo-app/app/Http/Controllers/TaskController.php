e2d4e36405381b84e7cfb2b342d956be8e34e821
Kobatai <taiga1983346@gmail.com>
2019-12-01 15:59:37 +0900

    taskコントローラーにsqlのメソッド追加

--- todo-app/app/Http/Controllers/TaskController.php
+++ todo-app/app/Http/Controllers/TaskController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use App\Folder;
+use App\Task;
 use Illuminate\Http\Request;
 
 class TaskController extends Controller
@@ -11,9 +12,14 @@ class TaskController extends Controller
     {
       $folders = Folder::all();
 
+      $current_folder = Folder::find($id);
+
+      $tasks = Taks::where('folder_id', $current_folder->id)->get();
+
       return view('tasks.index', [
           'folders' => $folders,
           'current_folder_id' => $id,
+          'tasks' => $tasks,
         ]);
     }
 }
