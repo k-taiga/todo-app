0d47e2fbef97cd088976414ec41798f51ef2b747
Kobatai <taiga1983346@gmail.com>
2019-12-01 16:52:26 +0900

    Taskモデルにattributeを追加

--- todo-app/app/Task.php
+++ todo-app/app/Task.php
@@ -7,4 +7,29 @@ use Illuminate\Database\Eloquent\Model;
 class Task extends Model
 {
     //
+  // 状態の定義
+
+  const STATUS = [
+    1 => ['label' => '未着手'],
+    2 => ['label' => '着手中'],
+    3 => ['label' => '完了'],
+  ];
+
+  // 状態のラベル
+  // @return string
+
+  public function getStatusLabelAttribute()
+  {
+    // 状態が入ったカラムから値を取得
+    $status = $this->attributes['status'];
+
+    // 定義されていなければ空文字を返す
+    if (!isset(self::STATUS[$status])) {
+      return '';
+    }
+
+    // 取得したDBの値から上で定義した状態のプロパティを返す
+    return self::STATUS[$status]['label'];
+
+  }
 }
--- todo-app/resources/views/tasks/index.blade.php
+++ todo-app/resources/views/tasks/index.blade.php
@@ -34,6 +34,37 @@
           </nav>
         </div>
         <div class="column col-md-8">
+          <div class="panel panel-default">
+            <div class="panel-heading">タスク</div>
+            <div class="panel-body">
+              <div class="text-right">
+                <a href="#" class="btn btn-default btn-block">
+                  タスクを追加する
+                </a>
+              </div>
+            <table class="table">
+              <thead>
+              <tr>
+                <th>タイトル</th>
+                <th>状態</th>
+                <th>期限</th>
+                <th></th>
+              </tr>
+              </thead>
+              <tbody>
+                @foreach($tasks as $task)
+              <tr>
+                <td>{{ $task->title }}</td>
+                <td>
+                  <span class="label">{{ $task->status_label}}</span>
+                </td>
+                <td>{{ $task->due_date}}</td>
+                <td><a href="#">編集</a></td>
+              </tr>
+                @endforeach
+              </tbody>
+            </table>
+          </div>
         </div>
     </div>
   </div>
