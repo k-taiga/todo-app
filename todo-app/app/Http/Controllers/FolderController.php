398af78d4ac928b935e86e27567da8278791601b
Kobatai <taiga1983346@gmail.com>
2019-12-01 17:54:06 +0900

    folderのcreateメソッド追加

--- todo-app/app/Http/Controllers/FolderController.php
+++ todo-app/app/Http/Controllers/FolderController.php
@@ -3,11 +3,29 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Folder;
 
 class FolderController extends Controller
 {
     public function showCreateForm()
     {
       return view('folders/create');
+    }
+
+    public function create(Request $request)
+    {
+
+      // Folderモデルのインスタンスを作成
+      $folder = new Folder();
+
+      // タイトルに入力値を代入する
+      $folder->title = $request->title;
+
+      // インスタンスをデータベースに保存する
+      $folder->save();
+
+      return redirect()->route('tasks.index', [
+        'id' => $folder->id,
+      ]);
     }
 }
