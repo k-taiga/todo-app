9c2eba206cace11cfc1d6e854e204a37b61619c0
Kobatai <taiga1983346@gmail.com>
2019-12-01 15:06:51 +0900

    create view

--- todo-app/app/Http/Controllers/TaskController.php
+++ todo-app/app/Http/Controllers/TaskController.php
@@ -2,12 +2,17 @@
 
 namespace App\Http\Controllers;
 
+use App\Folder;
 use Illuminate\Http\Request;
 
 class TaskController extends Controller
 {
     public function index()
     {
-      return "Hello world";
+      $folders = Folder::all();
+
+      return view('tasks.index', [
+          'folders' => $folders,
+        ]);
     }
 }
