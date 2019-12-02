46eeaad68cf4611325dc82359cfcfae69b059fa9
Kobatai <taiga1983346@gmail.com>
2019-12-02 22:03:16 +0900

    editのvalidationルール追加

---
+++ todo-app/app/Http/Requests/EditTask.php
@@ -0,0 +1,55 @@
+<?php
+
+namespace App\Http\Requests;
+
+use App\Task;
+use Illuminate\Foundation\Http\FormRequest;
+
+class EditTask extends CreateTask
+{
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array
+     */
+    public function rules()
+    {
+        $rule = parent::rules();
+
+        // inメソッドは入力値が許可リストに含まれているかチェックする
+        // 許可リストはTask::STATUSから配列としてキーを取得
+        $status_rule = Rule::in(array_keys(Task::STATUS));
+
+        return $rule + [
+            'status' => 'required|' . $status_rule
+            // required|in(1,2,3)
+        ];
+    }
+
+    public function attributes()
+    {
+        // 親のCreateTaskのattributesメソッドと合体させる
+        $attributes = parent::atributes();
+
+        return $attributes + [
+            'status' => '状態',
+        ];
+    }
+
+    public function messages()
+    {
+        $messages = parent::messages();
+
+        $status_labels = array_map(function($item) {
+            return $item['label'];
+        }, Task::STATUS);
+
+        $status_labels = implode(',', $status_labels);
+
+        return $messages + [
+            // status.inルールのメッセージを作成している
+            'status.in' => ':attribute には ' . $status_labels. 'のいずれかを指定してください。',
+        ];
+    }
+}
