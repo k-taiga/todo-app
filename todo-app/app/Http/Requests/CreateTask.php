66202dd05e8ec48b1d5aed143857b3957a97f8d8
Kobatai <taiga1983346@gmail.com>
2019-12-02 16:30:18 +0900

    validationのテスト実装

--- todo-app/app/Http/Requests/CreateTask.php
+++ todo-app/app/Http/Requests/CreateTask.php
@@ -24,7 +24,7 @@ class CreateTask extends FormRequest
     public function rules()
     {
         return [
-            'title' => 'required|max:100,'
+            'title' => 'required|max:100',
             'due_date' => 'required|date|after_or_equal:today',
         ];
     }
@@ -40,8 +40,8 @@ class CreateTask extends FormRequest
     public function messages()
     {
         return [
-            // キーでメッセージが表示されるべきルールを指定
-            // ドット区切りで左側が項目、右側がルール
+             // キーでメッセージが表示されるべきルールを指定する。
+            // ドット区切りで左側が項目、右側がルールを意味する。
             'due_date.after_or_equal' => ':attribute には今日以降の日付を入力してください。',
         ];
     }
--- todo-app/config/database.php
+++ todo-app/config/database.php
@@ -82,6 +82,23 @@ return [
             'prefix_indexes' => true,
         ],
 
+        // テスト環境用のdb
+        'mysql_testing' => [
+            'driver' => 'mysql',
+            'host' => env('DB_HOST', '127.0.0.1'),
+            'port' => env('DB_PORT', '3306'),
+            'database' => env('DB_DATABASE', 'forge'),
+            'username' => env('DB_USERNAME', 'forge'),
+            'password' => env('DB_PASSWORD', ''),
+            'unix_socket' => env('DB_SOCKET', ''),
+            'charset' => 'utf8mb4',
+            'collation' => 'utf8mb4_unicode_ci',
+            'prefix' => '',
+            'prefix_indexes' => true,
+            'strict' => true,
+            'engine' => null,
+        ],
+
     ],
 
     /*
