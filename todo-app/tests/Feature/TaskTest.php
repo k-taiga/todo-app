2e89d993b2ada94a737d9cad774575fb21660869
Kobatai <taiga1983346@gmail.com>
2019-12-02 22:14:04 +0900

    テストコード追加

--- todo-app/tests/Feature/TaskTest.php
+++ todo-app/tests/Feature/TaskTest.php
@@ -46,4 +46,23 @@ class TaskTest extends TestCase
             'due_date' => '期限日 には今日以降の日付を入力してください。',
         ]);
     }
+
+    /**
+     * 状態が定義された値ではない場合はバリデーションエラー
+     * @test
+     */
+    public function status_should_be_with_in_defined_numbers()
+    {
+        $this->seed('TasksTableSeeder');
+
+        $response = $this->post('/folders/1/tasks/1/edit', [
+            'title' => 'Sample task',
+            'due_date' => Carbon::today()->format('Y/m/d'),
+            'status' => 999,
+        ]);
+
+        $response->assertSeesionHasErrors([
+            'status' => '状態 には 未着手、着手中、完了 のいずれかを指定してください。',
+        ]);
+    }
 }
