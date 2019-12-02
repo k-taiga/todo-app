27508627c83209a630bf365d3fc28e913faff995
Kobatai <taiga1983346@gmail.com>
2019-12-02 12:00:27 +0900

    残りのviewの共通化

--- todo-app/resources/views/tasks/index.blade.php
+++ todo-app/resources/views/tasks/index.blade.php
@@ -1,19 +1,6 @@
-<!DOCTYPE html>
-<html lang="ja">
-<head>
-  <meta charset="UTF-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <meta http-equiv="X-UA-Compatible" content="ie=edge">
-  <title>ToDo App</title>
-  <link rel="stylesheet" href="/css/style.css">
-</head>
-<body>
-<header>
-  <nav class="my-navbar">
-    <a class="my-navbar-brand" href="/">ToDo App</a>
-  </nav>
-</header>
-<main>
+@extends('layout')
+
+@section('content')
   <div class="container">
     <div class="row">
       <div class="col col-md-4">
@@ -68,6 +55,4 @@
         </div>
     </div>
   </div>
-</main>
-</body>
-</html>
+@endsection
