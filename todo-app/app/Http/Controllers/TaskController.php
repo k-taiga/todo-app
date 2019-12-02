c5cc78a834b8297ecb3d0956ac9138c125962189
Kobatai <taiga1983346@gmail.com>
2019-12-02 17:48:33 +0900

    編集ページ作成

--- todo-app/app/Http/Controllers/TaskController.php
+++ todo-app/app/Http/Controllers/TaskController.php
@@ -34,6 +34,7 @@ class TaskController extends Controller
       ]);
     }
 
+    // POST /folders/{id}/tasks/create
     public function create(int $id, CreateTask $request)
     {
       $current_folder = Folder::find($id);
@@ -47,6 +48,16 @@ class TaskController extends Controller
 
       return redirect()->route('tasks.index', [
           'id' =>$current_folder->id,
+      ]);
+    }
+
+    // GET /folders/{id}/tasks/{task_id}/edit
+    public function showEditForm(int $id, int $task_id)
+    {
+      $task = Task::find($task_id);
+
+      return view('tasks/edit', [
+          'task' => $task,
       ]);
     }
 }
--- todo-app/routes/web.php
+++ todo-app/routes/web.php
@@ -1,7 +1,15 @@
 <?php
+// タスク一覧
 Route::get('/folders/{id}/tasks', 'TaskController@index')->name('tasks.index');
+
+// タスク登録
 Route::get('/folders/{id}/tasks/create', 'TaskController@showCreateForm')->name('tasks.create');
 Route::post('/folders/{id}/tasks/create', 'TaskController@create');
 
+// フォルダ登録
 Route::get('/folders/create', 'FolderController@showCreateForm')->name('folders.create');
 Route::post('/folders/create', 'FolderController@create');
+
+// タスク編集
+Route::get('/folders/{id}/tasks/{task_id}/edit', 'TaskController@showEditForm')->name('tasks.edit');
+Route::post('/folders/{id}/tasks/{tasks_id}/edit', 'TaskController@edit');
