15615a9c6555cce159c5dac132a0bd5c21d04a6f
Kobatai <taiga1983346@gmail.com>
2019-12-03 10:32:09 +0900

    viewのタイポ修正

--- todo-app/resources/views/tasks/index.blade.php
+++ todo-app/resources/views/tasks/index.blade.php
@@ -46,7 +46,7 @@
                   <span class="label {{ $task->status_class }}">{{ $task->status_label}}</span>
                 </td>
                 <td>{{ $task->formatted_due_date}}</td>
-                <td><a href="{{ route('tasks.edit, ['id' => $task->folder_id, 'task_id' => $task->id]) }}">編集</a></td>
+                <td><a href="{{ route('tasks.edit', ['id' => $task->folder_id, 'task_id' => $task->id]) }}">編集</a></td>
               </tr>
                 @endforeach
               </tbody>
