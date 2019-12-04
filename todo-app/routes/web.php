de44190ca0a5a67886746630380d1ee9549848d8
Kobatai <taiga1983346@gmail.com>
2019-12-04 09:07:40 +0900

    認証の追加

--- todo-app/routes/web.php
+++ todo-app/routes/web.php
@@ -1,22 +1,24 @@
 <?php
 
-// ホーム画面
-Route::get('/', 'HomeController@index')->name('home');
+Route::group(['middleware' => 'auth'], function() {
+  // ホーム画面
+  Route::get('/', 'HomeController@index')->name('home');
 
-// タスク一覧
-Route::get('/folders/{id}/tasks', 'TaskController@index')->name('tasks.index');
+  // タスク一覧
+  Route::get('/folders/{id}/tasks', 'TaskController@index')->name('tasks.index');
 
-// タスク登録
-Route::get('/folders/{id}/tasks/create', 'TaskController@showCreateForm')->name('tasks.create');
-Route::post('/folders/{id}/tasks/create', 'TaskController@create');
+  // タスク登録
+  Route::get('/folders/{id}/tasks/create', 'TaskController@showCreateForm')->name('tasks.create');
+  Route::post('/folders/{id}/tasks/create', 'TaskController@create');
 
-// フォルダ登録
-Route::get('/folders/create', 'FolderController@showCreateForm')->name('folders.create');
-Route::post('/folders/create', 'FolderController@create');
+  // フォルダ登録
+  Route::get('/folders/create', 'FolderController@showCreateForm')->name('folders.create');
+  Route::post('/folders/create', 'FolderController@create');
 
-// タスク編集
-Route::get('/folders/{id}/tasks/{task_id}/edit', 'TaskController@showEditForm')->name('tasks.edit');
-Route::post('/folders/{id}/tasks/{tasks_id}/edit', 'TaskController@edit');
+  // タスク編集
+  Route::get('/folders/{id}/tasks/{task_id}/edit', 'TaskController@showEditForm')->name('tasks.edit');
+  Route::post('/folders/{id}/tasks/{tasks_id}/edit', 'TaskController@edit');
+});
 
 // user認証周り
 Auth::routes();
