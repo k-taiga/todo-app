4a29d1cec29321dd9f5f328be535f9c5958ccea5
Kobatai <taiga1983346@gmail.com>
2019-12-04 09:03:36 +0900

    aタグでsubmitするためのJS追加

--- todo-app/resources/views/layout.blade.php
+++ todo-app/resources/views/layout.blade.php
@@ -31,6 +31,14 @@
 <main>
   @yield('content')
 </main>
+@if(Auth::check())
+  <script>
+    document.getElementById('logout').addEventListener('click', function(event) {
+      event.preventDefault();
+      document.getElementById('logout-form').submit();
+    })
+  </script>
+@endif
 @yield('scripts')
 </body>
 </html>
